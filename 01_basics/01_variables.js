07e0129e74ac92ac5f385927c1e492c517a05df7
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-16 06:57:04 +0000

    new commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail= "fromkhul@example.com"
+var accountPassword ="0123"
+accountCity ="Dhaka"
+
+//accountId = 999
+//console.log(accountId);//Assignment to constant variable ➡▶ once assigned value using const u can not change/alter it
+
+accountEmail = "kpc@fb.com"
+accountPassword ="889977"
+accountCity ="Magura"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
