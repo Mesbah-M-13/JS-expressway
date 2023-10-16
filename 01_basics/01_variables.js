9a75da75a341795500e70fa294723542e2a78b24
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-16 15:09:34 +0600

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 1234;
 let accountEmail = "fromkhul@example.com";
 var accountPassword = "0123";
 accountCity = "Dhaka";
-let accountState;// declared a variable without any value
+let accountState;// declared a variable without any value/initialization
 
 //accountId = 999
 //console.log(accountId);//Assignment to constant variable ➡▶ once assigned value using const u can not change/alter it
