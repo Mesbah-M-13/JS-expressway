06e19d16e665ef365f0131f65e55cef60eb114e2
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-16 08:18:32 +0000

    new commit done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,13 +1,19 @@
-const accountId = 1234
-let accountEmail= "fromkhul@example.com"
-var accountPassword ="0123"
-accountCity ="Dhaka"
+const accountId = 1234;
+let accountEmail = "fromkhul@example.com";
+var accountPassword = "0123";
+accountCity = "Dhaka";
+let accountState;// declared a variable without any value
 
 //accountId = 999
 //console.log(accountId);//Assignment to constant variable ➡▶ once assigned value using const u can not change/alter it
 
-accountEmail = "kpc@fb.com"
-accountPassword ="889977"
-accountCity ="Magura"
+accountEmail = "kpc@fb.com";
+accountPassword = "889977";
+accountCity = "Khulna";
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
