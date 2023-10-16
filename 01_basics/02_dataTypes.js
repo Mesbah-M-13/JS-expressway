fad9059fc291ae7984ae1c16954c7254c6205538
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-16 12:54:30 +0000

    dataTypes is done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version/modern syntax
+//alert(2+4) // using node js , not browser
+
+// JS original documentation link : https://tc39.es/ecma262/
+
+let name = "himel"; // string
+let age = 18; //number
+let isLoggedIn = false; // boolean
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof ''); // string
