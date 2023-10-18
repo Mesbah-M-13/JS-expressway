1609dd9a92dc30df0c6c6c0a510a105af4c2d7d6
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-18 09:48:00 +0000

    new code commit done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,87 @@
+// 1. number
+let score = 33;
+
+// console.log(typeof(score));
+// console.log(typeof score );
+
+// 2. string to number
+let scoreOne = "33abc"; //string that can't convert to number("Himel") will give NaN
+
+let valueInNumber1 = Number(scoreOne); // number conversion
+
+// console.log(scoreOne);
+// console.log(valueInNumber1);
+// console.log(typeof(valueInNumber1));
+// console.log(typeof valueInNumber1 );
+
+// 3. null to number
+let scoreTwo = null;
+
+let valueInNumber2 = Number(scoreTwo);
+
+// console.log(valueInNumber2);
+// console.log(typeof valueInNumber2);
+
+// 4. undefined to number
+let scoreThree = undefined;
+
+let valueInNumber3 = Number(scoreThree);
+
+// console.log(valueInNumber3);
+// console.log(typeof(valueInNumber3));
+
+// 5. boolean to number
+let scoreFour = true;
+
+let valueInNumber4 = Number(scoreFour);
+// console.log(valueInNumber4);
+// console.log(typeof valueInNumber4);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+// Boolean Conversion(b)
+// 1. num ==> (b)
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+
+// 2. empty string ==> (b)
+let isLoggedIn1 = ""
+
+let booleanIsLoggedIn1= Boolean(isLoggedIn1)
+
+console.log(booleanIsLoggedIn1);
+console.log(typeof(booleanIsLoggedIn));
+
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+// let score = 55;
+// let score_1 = "55abc";
+
+// console.log(typeof(score));
+// console.log(typeof score); // using method --> inside parenthesis
+
+// console.log(typeof(score_1));
+// console.log(typeof score_1);
+
+// let valueInNumber = Number(score_1)
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// console.log(typeof NaN); ==> number
+
+// let score_2 = null
+
+// let valueInNumber_2 = Number(score_2)
+// console.log(valueInNumber_2);
+// console.log(typeof valueInNumber_2);
