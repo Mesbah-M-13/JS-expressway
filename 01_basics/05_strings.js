e58a4dc9cc75a38a66e0f89c26ac16a47824f9c4
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-24 02:50:22 +0000

    strings and methods of String.prototype are done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,34 @@
-const name ="Himel"
-const repoCount = 20
+const name = "Himel";
+const repoCount = 20;
 
-console.log("I'm "+name +" and my repo count value is :-"+repoCount);
+// console.log("I'm "+name +" and repo count value :-"+repoCount);
+// console.log(`Hello I'm ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("mesbah-m13-com");
+
+console.log(gameName.length);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0, 4);
+//console.log(newString); // exclude the end index
+
+const anotherString = gameName.slice(-14, 6);
+//console.log(anotherString);// slice() extracts up to but not including indexEnd
+
+const newStringOne = "   mesbah    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://google.com/mesbah%20himel";
+
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("sundar"));
+
+console.log(gameName.split("-"));
+//The split() method splits a string into an array of substrings & returns the new array.
