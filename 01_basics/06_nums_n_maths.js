a5242ec63b7c1696c420717c5489192b114b173d
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-24 04:10:37 +0000

    number,math & their methods are done

--- 01_basics/06_nums_n_maths.js
+++ 01_basics/06_nums_n_maths.js
@@ -23,6 +23,8 @@ const hundreds = 1000000;
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
+// console.log(Math.trunc(13.307));
+
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
