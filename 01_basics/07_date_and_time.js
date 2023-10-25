7a4ca0fb20f260c462101d982cdce965773e4b00
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-25 01:20:45 +0000

    Date methods done

--- 01_basics/07_date_and_time.js
+++ 01_basics/07_date_and_time.js
@@ -1,7 +1,7 @@
-const myDate = new Date()
-//console.log(myDate);
-// console.log(myDate.toString());
-// console.log(myDate.toLocaleString());
+const myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toLocaleString());
 // console.log(myDate.toLocaleDateString());
 // console.log(myDate.toLocaleTimeString());
 // console.log(myDate.toDateString());
@@ -9,20 +9,20 @@ const myDate = new Date()
 // console.log(myDate.toISOString());
 // console.log(myDate.toJSON());
 
-
 // let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
+let myCreatedDate = new Date("01-14-2023");
 // console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now()
+//  ✅ ----- INTERVIEW-----✅ convert a date to milisec to sec
+let myTimeStamp = Date.now();
 
 // console.log(myTimeStamp);
 // console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(Math.floor(Date.now()/1000)); // milisec to sec
 
-let newDate = new Date()
+let newDate = new Date();
 //console.log(newDate);
 //console.log(newDate.getMonth() + 1); // to get the current month
 //console.log(newDate.getDay()); // // Sunday - Saturday : 0 - 6
@@ -31,10 +31,9 @@ let newDate = new Date()
 
 // `${newDate.getDay()} and the time `
 
-newDate.toLocaleString('default', {
-    weekday: "long",
-    
-})
+newDate.toLocaleString("default", {
+  weekday: "long",
+});
 
 // const anotherDate = new Date()
 // anotherDate.toLocaleString('default',{
@@ -45,8 +44,8 @@ newDate.toLocaleString('default', {
 // })
 // console.log(anotherDate.year);
 
-const date = new Date(Date.UTC(2023,0,22));
-console.log(date);
+const date = new Date(Date.UTC(2023, 0, 22));
+// console.log(date);
 // Request a weekday along with a long date
 const options = {
   weekday: "short",
@@ -54,8 +53,17 @@ const options = {
   month: "long",
   day: "numeric",
 };
+//console.log(date.toLocaleString("en-In", options));
 
-console.log(date.toLocaleString("en-In", options));
+let myDateTime = new Date();
+
+let myTime = new Date(myDateTime).toLocaleString("en-IN", {
+  hour: "2-digit",
+  minute: "2-digit",
+  timeZoneName: "long",
+});
+
+// console.log(myTime);
 
 /*
 to know more options: https://www.w3schools.com/jsref/jsref_tolocalestring.asp
