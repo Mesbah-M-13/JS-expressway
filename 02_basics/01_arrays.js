c2048d3a7c814138987322fd4abbebae9008184a
Mesbah-M-13 <139665223+Mesbah-M-13@users.noreply.github.com>
2023-10-26 02:56:16 +0000

    array with(slice,splice,push,pop,shift,unshift)✔

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -2,30 +2,6 @@
 // [] --> square brackets
 // {} --> curly braces
 
-// array
-
-// const myArr = [0, 1, 2, 3, 4, 5];
-// const myHeors = ["shaktiman", "naagraj"];
-
-// const myArr2 = new Array(1, 2, 3, 4);
-// console.log(myArr[1]);
-
-// Array methods
-
-// myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
-
-// myArr.unshift(9)
-// myArr.shift()
-
-// console.log(myArr.includes(9));
-// console.log(myArr.indexOf(3));
-
-// const newArr = myArr.join()
-
-// console.log(myArr);
-// console.log( newArr);
 
 // slice, splice
 
@@ -40,7 +16,8 @@
 // console.log("C ", myArr);
 // console.log(myn2);
 
-// Array --> Stores a collection of multiple items under a single variable name
+//✅ Array --> Stores a collection of multiple items under a single variable name
+
 /*
     1.JavaScript arrays are resizable and can contain a mix of different data types
 */
